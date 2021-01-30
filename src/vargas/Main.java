b73d074a3849c2271f37df84600f06ceb4632341
DiegoVargas <var20010@byui.edu>
2021-01-29 21:52:37 -0600

    Initial commit

---
+++ src/vargas/Main.java
@@ -0,0 +1,9 @@
+package vargas;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String message = new String("Hello my name is Diego Vargas.");
+	System.out.println(message);
+    }
+}
